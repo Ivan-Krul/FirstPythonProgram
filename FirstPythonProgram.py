d2ad68250b742eee24237563a2672a37910568f5
Ivan-Krul <kiwiivanya128@gmail.com>
2022-09-16 12:15:12 +0300

    write calculator

--- FirstPythonProgram.py
+++ FirstPythonProgram.py
@@ -1,2 +1,18 @@
 # Calculator
+a = float(input("A:"))
+sym = input("operator:")
+b = float(input("B:"))
 
+if sym == "+":
+    print(a+b)
+elif sym == "-":
+    print(a-b)
+elif sym == "*":
+    print(a*b)
+elif sym == "/":
+    if b == 0:
+        print("B mustn't be 0")
+    else:
+        print(a/b)
+else:
+    print("operator is not defined")
